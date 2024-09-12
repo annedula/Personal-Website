7a91003a32dbee9333e7bd750a41903bfeeea30d
annedla <jennyannedula4@gmail.com>
2024-09-12 12:09:17 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+$(".js-scroll-trigger").click(function () {
+  $(".navbar-collapse").collapse("hide");
+});
+var popoverTriggerList = [].slice.call(
+  document.querySelectorAll('[data-bs-toggle="popover"]')
+);
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl);
+});
